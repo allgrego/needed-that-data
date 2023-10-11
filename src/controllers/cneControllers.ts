9119f4b8ad6a24dcdce668823ebb400d9a545548
Gregorio Alvarez <galvarez@cbpi.com.mx>
2023-10-10 20:57:33 -0400

    [fix] Improve error handling in CNE data fetching controller

--- src/controllers/cneControllers.ts
+++ src/controllers/cneControllers.ts
@@ -7,69 +7,83 @@ import { HttpErrorResponse } from "../types/errors.types";
 import { getPersonByCid } from "../utils/cne/fetch";
 import { PersonDataCne } from "../utils/cne/fetch.types";
 import { getRifFromCid } from "../utils/cne/rif";
-import { cidNumberIsValid, nationalityIsValid, parseCidNumber, parseNationality } from "../utils/cne/validations";
-
+import {
+  cidNumberIsValid,
+  nationalityIsValid,
+  parseCidNumber,
+  parseNationality,
+} from "../utils/cne/validations";
 
 // Get person info by their cedula ID
 export const getInfoByCID = async (req: Request, res: Response) => {
-    // Standard error response
-    const error: HttpErrorResponse = {
-        error: {
-            code: 'internal',
-            message: 'INTERNAL'
-        }
+  // Standard error response
+  const error: HttpErrorResponse = {
+    error: {
+      code: "internal",
+      message: "INTERNAL",
+    },
+  };
+
+  let status = 500;
+
+  try {
+    // Cache
+    // res.set("Cache-Control", "public, max-age=30, s-maxage=60");
+
+    const providedNationality: string | undefined =
+      String(req.query?.nat) || undefined;
+    const providedNumber: string | undefined =
+      String(req.query?.num) || undefined;
+
+    if (!nationalityIsValid(providedNationality)) {
+      error.error.code = "invalid-argument";
+      error.error.message =
+        "'nat' argument (nationality) is missing or invalid (must be 'V' or 'E')";
+      status = 400;
+      throw new Error("Invalid nationality");
     }
 
-    try {
-        // Cache
-        // res.set("Cache-Control", "public, max-age=30, s-maxage=60");
-
-        const providedNationality: string | undefined = String(req.query?.nat) || undefined
-        const providedNumber: string | undefined = String(req.query?.num) || undefined
-
-        if (!nationalityIsValid(providedNationality)) {
-            error.error.code = 'invalid-argument'
-            error.error.message = '\'nat\' argument (nationality) is missing or invalid (must be \'V\' or \'E\')'
-            res.status(400).json(error)
-            return
-        }
-
-        if (!cidNumberIsValid(providedNumber)) {
-            error.error.code = 'invalid-argument'
-            error.error.message = '\'num\' argument (cedula number) is missing or invalid (must be in numeric format \'1234567\')'
-            res.status(400).json(error)
-            return
-        }
-
-        const nationality = parseNationality(providedNationality)
-        const number = parseCidNumber(providedNumber)
-
-        // Get person data from CNE API
-        const person: PersonDataCne | undefined | 'fetching-error' = await getPersonByCid(nationality, number)
-
-        // Fetching error
-        if (person === 'fetching-error') {
-            error.error.code = 'internal'
-            error.error.message = 'There was an internal error fetching the data'
-            res.status(500).json(error)
-            return
-        }
-
-        // Content verification
-        if (!person) {
-            error.error.code = 'not-found'
-            error.error.message = 'No data record was found for provided parameters'
-            res.status(404).json(error)
-            return
-        }
-
-        // Calculate the person RIF according to the CID
-        person.rif = getRifFromCid(nationality, number)
-
-        res.json(person)
-        return
-    } catch (err) {
-        console.log(err);
-        res.json(error)
+    if (!cidNumberIsValid(providedNumber)) {
+      error.error.code = "invalid-argument";
+      error.error.message =
+        "'num' argument (cedula number) is missing or invalid (must be in numeric format '1234567')";
+      status = 400;
+      throw new Error("Invalid CID number");
     }
-}
+
+    const nationality = parseNationality(providedNationality);
+    const number = parseCidNumber(providedNumber);
+
+    // Get person data from CNE API
+    const person: PersonDataCne | undefined | "fetching-error" =
+      await getPersonByCid(nationality, number);
+
+    // Fetching error
+    if (person === "fetching-error") {
+      error.error.code = "internal";
+      error.error.message =
+        "There was an error fetching the data. Service is not available";
+      status = 503;
+      throw new Error("Error in fetching result");
+    }
+
+    // Content verification
+    if (!person) {
+      error.error.code = "not-found";
+      error.error.message = "No data record was found for provided parameters";
+      status = 404;
+      throw new Error("No data found for provided parameters");
+    }
+
+    status = 200;
+
+    // Calculate the person RIF according to the CID
+    person.rif = getRifFromCid(nationality, number);
+
+    res.status(status).json(person);
+    return;
+  } catch (err) {
+    console.log("Failure in getInfoByCID controller:", err);
+    res.status(status).json(error);
+  }
+};
--- src/utils/cne/fetch.ts
+++ src/utils/cne/fetch.ts
@@ -1,125 +1,141 @@
-import fetch from "cross-fetch"
-import { parse, HTMLElement } from 'node-html-parser'
-import { PersonDataCne } from "./fetch.types"
-import { contentIsValid } from "./validations"
+import fetch from "cross-fetch";
+import { HTMLElement, parse } from "node-html-parser";
 
+import { contentIsValid } from "./validations";
 
-export const getPersonByCid = async (nationality: string, number: string): Promise<PersonDataCne | undefined | 'fetching-error'> => {
+import { PersonDataCne } from "./fetch.types";
+
+export const getPersonByCid = async (
+  nationality: string,
+  number: string
+): Promise<PersonDataCne | undefined | "fetching-error"> => {
+  const FETCHING_ERROR_CODE = "fetching-error";
+  try {
     /**
-         * Fetch data from CNE 
-         */
-    const url = `http://www.cne.gob.ve/web/registro_electoral/ce.php?nacionalidad=${nationality}&cedula=${number}`
+     * Fetch data from CNE
+     */
+    const url = `http://www.cne.gob.ve/web/registro_electoral/ce.php?nacionalidad=${nationality}&cedula=${number}`;
 
-    const fetchResponse = await fetch(url)
+    console.log(`Fetching from "${url}"`);
 
-    if (!fetchResponse.ok) return 'fetching-error'
+    const fetchResponse = await fetch(url);
 
-    const textResponse = await fetchResponse.text()
+    const textResponse = await fetchResponse.text();
+
+    if (!fetchResponse.ok) {
+      console.log("Server error response", textResponse);
+      return FETCHING_ERROR_CODE;
+    }
 
     // Parse html
     const root: HTMLElement = parse(textResponse);
     // With normalized spaces
-    const rawText: string = root.rawText.replace(/\s+/gmi, ' ').trim()
+    const rawText: string = root.rawText.replace(/\s+/gim, " ").trim();
 
     // Has name verification
-    if (!contentIsValid(rawText)) return undefined
+    if (!contentIsValid(rawText)) return undefined;
 
     // Get name
-    const name = getNameFromText(rawText)
-    const state = getStateFromText(rawText)
-    const municipality = getMunicipalityFromText(rawText)
-    const parish = getParishFromText(rawText)
+    const name = getNameFromText(rawText);
+    const state = getStateFromText(rawText);
+    const municipality = getMunicipalityFromText(rawText);
+    const parish = getParishFromText(rawText);
 
     const personData: PersonDataCne = {
-        name: name || 'none',
-        state,
-        municipality,
-        parish,
-    }
+      name: name || "none",
+      state,
+      municipality,
+      parish,
+    };
 
-    return personData
-}
+    return personData;
+  } catch (error) {
+    console.error("Failure getting a person data by their CID", error);
+    return FETCHING_ERROR_CODE;
+  }
+};
 
 /**
  * Get name from raw text content
- * 
- * @param textContent 
- * @returns 
+ *
+ * @param textContent
+ * @returns
  */
 export const getNameFromText = (textContent?: string) => {
-    if (!textContent) return undefined
+  if (!textContent) return undefined;
 
-    const nameMatches = textContent.match(/\s+nombre:\s+(.+)\s+estado:\s+/gmi)
+  const nameMatches = textContent.match(/\s+nombre:\s+(.+)\s+estado:\s+/gim);
 
-    if (!nameMatches) return undefined
+  if (!nameMatches) return undefined;
 
-    let [name] = nameMatches
-    // Remove prev
-    name = name.replace(/\s+nombre:\s+/gmi, '')
-    // Remove post
-    name = name.replace(/\s+estado:\s+/gmi, '')
-    // Normalize spaces
-    name = name.replace(/\s+/gmi, ' ')
+  let [name] = nameMatches;
+  // Remove prev
+  name = name.replace(/\s+nombre:\s+/gim, "");
+  // Remove post
+  name = name.replace(/\s+estado:\s+/gim, "");
+  // Normalize spaces
+  name = name.replace(/\s+/gim, " ");
 
-    return name
-}
-
+  return name;
+};
 
 export const getStateFromText = (textContent?: string) => {
-    if (!textContent) return undefined
+  if (!textContent) return undefined;
 
-    const stateMatches = textContent.match(/\s+estado:\s+(.+)\s+municipio:/gmi)
+  const stateMatches = textContent.match(/\s+estado:\s+(.+)\s+municipio:/gim);
 
-    if (!stateMatches) return undefined
+  if (!stateMatches) return undefined;
 
-    let [state] = stateMatches
+  let [state] = stateMatches;
 
-    // Remove prev
-    state = state.replace(/\s+estado:\s+/gmi, '')
-    // Remove post
-    state = state.replace(/\s+municipio:/gmi, '')
-    // Normalize spaces
-    state = state.replace(/\s+/gmi, ' ')
+  // Remove prev
+  state = state.replace(/\s+estado:\s+/gim, "");
+  // Remove post
+  state = state.replace(/\s+municipio:/gim, "");
+  // Normalize spaces
+  state = state.replace(/\s+/gim, " ");
 
-    return state
-}
-
+  return state;
+};
 
 export const getMunicipalityFromText = (textContent?: string) => {
-    if (!textContent) return undefined
+  if (!textContent) return undefined;
 
-    const municipalityMatches = textContent.match(/\s+municipio:\<.*\>\s+(.+)\s+parroquia:/gmi)
+  const municipalityMatches = textContent.match(
+    /\s+municipio:\<.*\>\s+(.+)\s+parroquia:/gim
+  );
 
-    if (!municipalityMatches) return undefined
+  if (!municipalityMatches) return undefined;
 
-    let [municipality] = municipalityMatches
+  let [municipality] = municipalityMatches;
 
-    // Remove prev
-    municipality = municipality.replace(/\s+municipio:\<.*\>\s+/gmi, '')
-    // Remove post
-    municipality = municipality.replace(/\s+parroquia:/gmi, '')
-    // Normalize spaces
-    municipality = municipality.replace(/\s+/gmi, ' ')
+  // Remove prev
+  municipality = municipality.replace(/\s+municipio:\<.*\>\s+/gim, "");
+  // Remove post
+  municipality = municipality.replace(/\s+parroquia:/gim, "");
+  // Normalize spaces
+  municipality = municipality.replace(/\s+/gim, " ");
 
-    return municipality
-}
-
+  return municipality;
+};
 
 export const getParishFromText = (textContent?: string) => {
-    if (!textContent) return undefined
+  if (!textContent) return undefined;
 
-    const municipalityMatches = textContent.match(/\s+parroquia:\s+(.+)\s+centro:/gmi)
+  const municipalityMatches = textContent.match(
+    /\s+parroquia:\s+(.+)\s+centro:/gim
+  );
 
-    if (!municipalityMatches) return undefined
+  if (!municipalityMatches) return undefined;
 
-    let [municipality] = municipalityMatches
+  let [municipality] = municipalityMatches;
 
-    // Remove prev
-    municipality = municipality.replace(/\s+parroquia:\s+/gmi, '')
-    // Remove post
-    municipality = municipality.replace(/\s+centro:/gmi, '')
-    // Normalize spaces
-    municipality = municipality.replace(/\s+/gmi, ' ')
+  // Remove prev
+  municipality = municipality.replace(/\s+parroquia:\s+/gim, "");
+  // Remove post
+  municipality = municipality.replace(/\s+centro:/gim, "");
+  // Normalize spaces
+  municipality = municipality.replace(/\s+/gim, " ");
 
-    return municipality
-}
+  return municipality;
+};
